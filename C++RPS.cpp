cc73da88b1d12ff3927005a910fdc399d23811ec
Gabriel Gary <gabrielgary@outlook.com>
2024-10-13 13:09:29 +0100

    Update C++RPS.cpp
    Housekeeping
    Added comments, moved some stuff around

--- C++RPS.cpp
+++ C++RPS.cpp
@@ -6,31 +6,33 @@ class Player{
 		int health;
 		string name;
 	public:
-	    Player(int structHealth, string structName){
-	        alive = true;
-		    health = structHealth;
-		    name = structName;
+	    Player(int structHealth, string structName){ // Constructor
+	        alive = true; // alive will always be true by default
+		health = structHealth;
+		name = structName;
 	    }
-		void takeLife(){
+		// SET methods
+		void takeLife(){ // Takes away 1 life
 			health -= 1;
 			return;
 		}
-		int getLives(){
-			return health;
-		}
-		bool isAlive(){
-			return alive;
+		void setName(string newName){
+			name = newName;
+			return;
 		}
 		void kill(){
 			alive = false;
 			return;
 		}
+		// GET methods
+		bool isAlive(){
+			return alive;
+		}
+		int getLives(){
+			return health;
+		}
 		string getName(){
 			return name;
-		}
-		void setName(string newName){
-			name = newName;
-			return;
 		}
 };
 string counterAttack(int debug){ // Get the CPU's move
@@ -47,19 +49,21 @@ string counterAttack(int debug){ // Get the CPU's move
 	else if(selection == 1){
 		return("P");
 	}
-	else if(selection == 2){ // Number will be between 0 and 2 thanks to the module is of 3 
+	else if(selection == 2){ // Number will be between 0 and 2 thanks to the modulo is of 3 
 		return("S");
 	}
 	else{ // This should never without debug
-		cerr << "\033[31m\tERROR Code: \t\"How did we get here\"\033[0m" << endl;
+		cerr << "\033[31m\tERROR Code: \t\"How did we get here\"\033[0m" << endl; // \033[31m and \033[0m will change the colour of the text, I need to look into this more
 		return("How did we get here");
 	}
 }
-string didPlayerWin(string playerChoice, string cpuChoice){
+string didPlayerWin(string playerChoice, string cpuChoice){ // Function to return whether the player won or lost
+	// Draw state
 	if(playerChoice == cpuChoice){
 		cout << "CPU Picked: " << cpuChoice << endl;
 		return "Draw";
 	}
+	// Win States
 	else if(playerChoice == "R" && cpuChoice == "S"){
 		cout << "CPU Picked: " << cpuChoice << endl;		
 		return "Win";
@@ -72,6 +76,7 @@ string didPlayerWin(string playerChoice, string cpuChoice){
 		cout << "CPU Picked: " << cpuChoice << endl;
 		return "Win";
 	}
+	// Loss states
 	else if(cpuChoice == "R" && playerChoice == "S"){
 		cout << "CPU Picked: " << cpuChoice << endl;
 		return "Loss";
@@ -84,25 +89,27 @@ string didPlayerWin(string playerChoice, string cpuChoice){
 		cout << "CPU Picked: " << cpuChoice << endl;
 		return "Loss";
 	}
-	else{
+	else{ // This should never run, idk why I put it here
 		return "Idk man";
 	}
 }
 int main(){
+	// Initial set up
 	string playername;
-	int debug = 1;
+	int debug = 1; // If this variable is odd debug is off, if not it's on and will force an error.
 	cout << "Welcome! Please enter a name" << endl;
-	cin >> playername;
+	cin >> playername; // Set the player's name to be 
 	Player player(3, playername);
 	int coins = 0;
 	int wins = 0;
 	string selection = "B";
-	while(player.isAlive() == true){
-		cout << "Coins: " << coins << "\tWins: " << wins << endl;
+	// End of initial set up
+	while(player.isAlive() == true){ // Main game loop
+		cout << "Coins: " << coins << "\tWins: " << wins << endl; // Displays current run information
 		cout << "Please pick Rock (R) Paper (P) or Scissors (S)" << endl;
 		cin >> selection;
 		if(selection == "R" || selection == "P" || selection == "S"){
-			string state = didPlayerWin(selection, counterAttack(debug));
+			string state = didPlayerWin(selection, counterAttack(debug)); // Check if the player win or not or if it was a draw
 			if(state == "Draw"){
 				cout << "It's a draw :/" << endl;
 			}
@@ -122,20 +129,21 @@ int main(){
 			}
 		}
 		else if(selection == "debug"){
-			debug += 1;
-			cout << "Debug state: " << debug << "\t(Odd = Off\t Even = On)" << endl;
+			debug += 1; // Changes debug by +1, much easier and smaller than having it toggle, though I understand that toggle variables could also be a good idea
+			cout << "Debug state: " << debug << "\t(Odd = Off\t Even = On)" << endl; // Output debug state
 		}
 		else {
 			player.takeLife();
 			cout << "Invalid input! -1 Life\n" << player.getLives() << " Lives left" << endl;
 		}
 		if(player.getLives() <= 0){
-			cout << "\tOut of lives!" << endl;
+			// Game over screen
+			cout << "\tGAME OVER" << endl;
 			cout << "\tPlayer: " << player.getName() << endl;
 			cout << "\t---\tSCORE\t---" << endl;
 			cout << "\tCoins: \t" << coins << endl;
 			cout << "\tWins:  \t" << wins << endl;
-			cin;
+			cin; // This makes sure that the program doesn't close unless the user makes an input.
 			player.kill();
 		}
 	}
